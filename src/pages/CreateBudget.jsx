06c83b28a64a7fb57d1ff4d139e021337bf319f8
ajcaro <antoniojcaro@gmail.com>
2022-03-15 18:45:12 +0100

    formulario, validaciones, calculos y submit

--- src/pages/CreateBudget.jsx
+++ src/pages/CreateBudget.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CreateBudget = () => {
 	const [values, setValues] = useState({
 		customer: '',
 		cif: '',
 		date: new Date().toISOString().substring(0, 10),
+		amount: 0,
+		tax: 0.21,
 	});
 
 	const [validFields, setValidFields] = useState({
 		customer: { valid: false, message: '*' },
 		cif: { valid: false, message: '*' },
 	});
+
+	const [calculateFields, setCalculateFields] = useState({
+		taxAmount: 0,
+		totalBudget: 0,
+	});
+
+	const [isValidForm, setIsValidForm] = useState(false);
 
 	const validateInput = (name, value) => {
 		switch (name) {
@@ -72,9 +81,28 @@ const CreateBudget = () => {
 		});
 	};
 
+	useEffect(() => {
+		const taxAmount = values.amount * values.tax;
+		const totalBudget = Number(values.amount) + Number(taxAmount);
+		setCalculateFields({ taxAmount, totalBudget });
+	}, [values.amount, values.tax]);
+
+	useEffect(() => {
+		if (validFields.customer.valid && validFields.cif.valid) {
+			setIsValidForm(true);
+		} else {
+			setIsValidForm(false);
+		}
+	}, [validFields]);
+
+	const handleOnSubmit = e => {
+		e.preventDefault();
+		console.log(values);
+	};
+
 	return (
 		<div className='container'>
-			<form>
+			<form onSubmit={handleOnSubmit}>
 				<div className='row'>
 					<div className='col-100'>
 						<label>
@@ -113,6 +141,56 @@ const CreateBudget = () => {
 							onChange={handleOnChange}
 						/>
 					</div>
+				</div>
+
+				<div className='row'>
+					<div className='col-50'></div>
+					<div className='col-50'>
+						<label>Importe presupuesto</label>
+						<input
+							type='number'
+							name='amount'
+							value={values.amount}
+							onChange={handleOnChange}
+						/>
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col-50'></div>
+					<div className='col-50'>
+						<label>% IVA</label>
+						<select name='tax' value={values.tax} onChange={handleOnChange}>
+							<option value={0}>0 %</option>
+							<option value={0.4}>4 %</option>
+							<option value={0.1}>10 %</option>
+							<option value={0.21}>21 %</option>
+						</select>
+					</div>
+				</div>
+
+				<div className='row'>
+					<div className='col-50'></div>
+					<div className='col-50'>
+						<label>Importe IVA</label>
+						<input type='number' value={calculateFields.taxAmount} readOnly />
+					</div>
+				</div>
+
+				<div className='row'>
+					<div className='col-50'></div>
+					<div className='col-50'>
+						<label>Total Presupuesto</label>
+						<input
+							type='number'
+							value={calculateFields.totalBudget}
+							readOnly
+						/>
+					</div>
+				</div>
+				<div className='row end'>
+					<button disabled={!isValidForm} type='submit'>
+						Enviar
+					</button>
 				</div>
 			</form>
 		</div>
